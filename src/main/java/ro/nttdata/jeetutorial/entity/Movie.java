cd75a579a413c01533fc5a0391fd39afdc231e1f
Sorin Dabiste <sorin0187@gmail.com>
2019-02-11 17:33:22 +0200

    added missing @Entity annotation

--- src/main/java/ro/nttdata/jeetutorial/entity/Movie.java
+++ src/main/java/ro/nttdata/jeetutorial/entity/Movie.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.sun.javafx.beans.IDProperty;
-
+@Entity
 @Table(name = "T_MOVIES")
 public class Movie {
 
@@ -54,3 +54,4 @@ public class Movie {
         this.id = id;
     }
 }
+
